9a1add120d5ef63912f5203c4991962ec8a7570c
eYuM-coder <nathaniel2007w@gmail.com>
2023-08-03 15:30:29 +0000

    fix styles hopefully

--- client/script.js
+++ client/script.js
@@ -44,13 +44,13 @@ function chatStripe(isAi, value, uniqueId) {
     `
       <div class="wrapper ${isAi && 'ai'}">
         <div class="chat">
-          <div className="profile">
-            <img
-            src="${isAi ? bot : user}"
-            alt="${isAi ? 'bot' : 'user'}"
+          <div class="profile">
+            <img 
+              src=${isAi ? bot : user}
+              alt="${isAi ? 'bot' : 'user'}"
             />
           </div>
-          <div className="message" id=${uniqueId}>${value}</div>
+          <div class="message" id=${uniqueId}>${value}</div>
         </div>
       </div>
     `
