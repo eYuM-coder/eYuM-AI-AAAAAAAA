ba27d3c69e7635117b2849a325a6cf230520e2ca
eYuM-coder <nathaniel2007w@gmail.com>
2024-01-06 23:32:08 +0000

    Update

--- client/script.js
+++ client/script.js
@@ -77,54 +77,52 @@ function chatStripe(isAi, value, uniqueId) {
 }
 
 const handleSubmit = async (e) => {
-  if(e.keyCode === 13 && !(e.keyCode === 16)) {
-    e.preventDefault()
+  e.preventDefault()
 
-    const data = new FormData(form)
+  const data = new FormData(form)
 
-    // user's chatstripe
-    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
 
-    // to clear the textarea input 
-    form.reset()
+  // to clear the textarea input 
+  form.reset()
 
-    // bot's chatstripe
-    const uniqueId = generateUniqueId()
-    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+  // bot's chatstripe
+  const uniqueId = generateUniqueId()
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
 
-    // to focus scroll to the bottom 
-    chatContainer.scrollTop = chatContainer.scrollHeight;
+  // to focus scroll to the bottom 
+  chatContainer.scrollTop = chatContainer.scrollHeight;
 
-    // specific message div 
-    const messageDiv = document.getElementById(uniqueId)
+  // specific message div 
+  const messageDiv = document.getElementById(uniqueId)
 
-    // messageDiv.innerHTML = "..."
-    loader(messageDiv)
+  // messageDiv.innerHTML = "..."
+  loader(messageDiv)
 
-    const response = await fetch('https://eyumaiserver.onrender.com', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-          prompt: data.get('prompt')
-      })
+  const response = await fetch('https://eyumaiserver.onrender.com', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        prompt: data.get('prompt')
     })
+  })
 
-    clearInterval(loadInterval)
-    messageDiv.innerHTML = " "
+  clearInterval(loadInterval)
+  messageDiv.innerHTML = " "
 
-    if (response.ok) {
-      const data = await response.json();
-      const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
 
-      typeText(messageDiv, parsedData)
-    } else {
-      const err = await response.text()
+    typeText(messageDiv, parsedData)
+  } else {
+    const err = await response.text()
 
-      messageDiv.innerHTML = "Something went wrong"
-      alert(err)
-    }
+    messageDiv.innerHTML = "Something went wrong"
+    alert(err)
   }
 }
 
@@ -166,14 +164,11 @@ themeChanger.addEventListener('click', () => {
   }
 });
 form.addEventListener('keyup', (e) => {
-  if (e.keyCode === 13) {
-    handleSubmit(e);
+  if (e.keyCode === 13 && e.shiftKey) {
+    e.preventDefault();
   } else if (e.keyCode === 18) {
     e.preventDefault();
-  }
-})
-form.addEventListener('keydown', (e) => {
-  if (e.keyCode === 16) {
+  } else if (e.keyCode === 13) {
     handleSubmit(e);
   }
 })
