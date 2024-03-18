f032fdfbb941f61268280fafb7e1d6d091329c4d
eYuM-coder <nathaniel2007w@gmail.com>
2024-03-18 18:44:10 +0000

    a

--- server/server.js
+++ server/server.js
@@ -2,7 +2,10 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import OpenAI from 'openai';
+import axios from 'axios';
 dotenv.config();
+
+const openaiEndpoint = 'https://api.openai.com/v1/chat/completions';
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -22,16 +25,28 @@ app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
-    const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: 'user', content: prompt }],
-    });
+    const response = await axios.post(
+      openaiEndpoint,
+      {
+        model: 'gpt-3.5-turbo',
+        messaages: [
+          { role: 'user', content: prompt},
+        ],
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+      }
+    );
 
       res.status(200).send({
-        bot: response.data.choices[0].message.content,
+        bot: response.data.choices[0].message.content;
       })
   } catch (error) {
     console.log(error);
+    
     res.status(500).send({ error });
   }
 });
