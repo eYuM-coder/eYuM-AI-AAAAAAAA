3f6e3121449e7c65d2c84d6e7cbc9a163102babf
eYuM-coder <nathaniel2007w@gmail.com>
2024-03-18 18:05:36 +0000

    a

--- server/server.js
+++ server/server.js
@@ -26,10 +26,10 @@ app.post('/', async (req, res) => {
     const prompt = req.body.prompt;
 
     const response = await openai.createCompletion({
-      model: "text-gpt3",
+      model: "gpt-3.5-turbo",
       prompt: `${prompt}`,
       temperature: 0,
-      max_tokens: 3000,
+      max_tokens: 16000,
       top_p: 1,
       frequency_penalty: 0.5,
       presence_penalty: 0
