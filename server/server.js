326a2f47bcea0d6684612a7eadea9c4a65c44c4e
eYuM-coder <nathaniel2007w@gmail.com>
2024-03-18 18:31:56 +0000

    a

--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 dotenv.config();
 
-const configuration = new Configuration({
+const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
-
-const openai = new OpenAIApi(configuration);
 
 const app = express();
 app.use(cors());
@@ -25,18 +22,13 @@ app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
-    const response = await openai.createCompletion({
+    const response = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
-      prompt: `${prompt}`,
-      temperature: 0,
-      max_tokens: 16000,
-      top_p: 1,
-      frequency_penalty: 0.5,
-      presence_penalty: 0
+      messages: [{ role: 'user', content: prompt }],
     });
 
       res.status(200).send({
-        bot: response.data.choices[0].text
+        bot: response.data.choices[0].message.content,
       })
   } catch (error) {
     console.log(error);
